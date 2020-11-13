fe9c6c593979d17b1230b93d6bcf6adb08f003ec
Slavik lopush <slavik.lopush@gmail.com>
2020-11-13 16:17:07 +0200

    minor cahnges

--- src/Containers/NewsItem.js
+++ src/Containers/NewsItem.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const imgStyle = {
-  hight: "auto",
-  width: "80%",
-  border: "4px solid RebeccaPurple ",
-  borderRadius: "5%"
-};
-
 const articleStyle = {
   width: "50%",
   margin: "0 auto",
@@ -16,13 +9,29 @@ const articleStyle = {
 
 let NewsItem = ({ article }) =>
   article ? (
-    <article style={articleStyle}>
-      <div>
-        <h1>{article.title}</h1>
-        <h4>{article.id}</h4>
-        
-      </div>
-    </article>
+    <div>
+      <article style={articleStyle}>
+        <div>
+          <h1>{article[0].title}</h1>
+          <h4>{article[0].id}</h4>
+          
+        </div>
+      </article>
+      <article style={articleStyle}>
+        <div>
+          <h1>{article[1].title}</h1>
+          <h4>{article[1].id}</h4>
+          
+        </div>
+      </article>
+      <article style={articleStyle}>
+        <div>
+          <h1>{article[2].title}</h1>
+          <h4>{article[2].id}</h4>
+          
+        </div>
+      </article>
+    </div>
   ) : null;
 
 const mapStateToProps = state => ({
